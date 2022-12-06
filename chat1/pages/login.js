cb651291293070db509f93529a77dbaf7448850b
KamoEllen <108501662+KamoEllen@users.noreply.github.com>
2022-12-06 17:34:19 +0000

    Pending changes exported from your codespace

---
+++ chat1/pages/login.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import Head from 'next/head'
+import styled from 'styled-components'
+
+const login = () => {
+  return (
+    <Container>
+      <Head>
+        <title>Login</title>
+      </Head>
+    </Container>
+  )
+}
+
+export default login;
+
+const Container = styled.div``;
